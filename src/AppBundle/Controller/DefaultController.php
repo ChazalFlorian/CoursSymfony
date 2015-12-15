82718e02e1f3c5624039faa1f799cb5fccb23667
Chazallapedale <Chazal.florian@gmail.com>
2015-12-15 23:14:47 +0100

    added CRUD without R for Post

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,53 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
+
+    public function addAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        $em = $this->getDoctrine()->getManager();
+
+        $post = new Post();
+        $post->setTitle("Coucou les coupaings!");
+        $post->setBody("Ceci est un article de test dans le cadre du cours Symfony a Sup\'Internet. <br> Il ne contient rien");
+        $post->setIsPublished(true);
+        $em->persist($post);
+        $em->flush();
+
+        return $this->render('AppBundle:default:addPost.html.twig');
+    }
+
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $emRepo = $this->getDoctrine()->getManager()->getRepository('AppBundle:Post');
+        //$post = new Post();
+        $post = $emRepo->findOneById($id);
+        $post->setTitle("Modification du Titre");
+        $em->persist($post);
+        $em->flush();
+
+        return $this->render('AppBundle:default:editPost.html.twig', array(
+            'id' => $id
+        ));
+    }
+
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $emRepo = $this->getDoctrine()->getManager()->getRepository('AppBundle:Post');
+        $post = $emRepo->findOneById($id);
+        $em->remove($post);
+        $em->flush();
+
+        return $this->render('AppBundle:default:deletePost.html.twig', array(
+            'id' => $id
         ));
     }
 }
